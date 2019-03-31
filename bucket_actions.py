265960ccf30e974c53f597963c70eff4998279c8
semmaow <semmaow@gmail.com>
2019-03-31 17:27:52 -0600

    removed randomness from create_temp_file

--- bucket_actions.py
+++ bucket_actions.py
@@ -4,9 +4,6 @@
 import boto3
 import uuid
 
-s3_resource = boto3.resource('s3')
-
-
 def create_bucket_name(bucket_prefix):
     return ''.join([bucket_prefix, str(uuid.uuid4())])
 
@@ -16,16 +13,13 @@ def create_bucket(bucket_prefix, s3_connection):
     bucket_name = create_bucket_name(bucket_prefix)
     bucket_response = s3_connection.create_bucket(
             Bucket=bucket_name,
-            CreateBucketConfiguration={
-                'LocationConstraint': current_region})
+            CreateBucketConfiguration={'LocationConstraint': current_region})
     print(bucket_name, current_region)
     return bucket_name, bucket_response
 
 def create_temp_file(size, file_name, file_content):
-    random_file_name = ''.join([str(uuid.uuid4().hex[:6]), file_name])
-    with open(random_file_name, 'w') as f:
+    with open(file_name, 'w') as f:
         f.write(str(file_content) * size)
-    return random_file_name
 
 def copy_to_bucket(bucket_from_name, bucket_to_name, file_name):
     copy_source = {

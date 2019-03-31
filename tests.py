486546d6d1109f6991d2756f43acd3a0c08b88e2
semmaow <semmaow@gmail.com>
2019-03-31 14:10:49 -0600

    created test skeleton

---
+++ tests.py
@@ -0,0 +1,30 @@
+import unittest
+import bucket_actions
+
+
+class BucketTests(unittest.TestCase):
+
+    # can reach AWS
+
+
+
+    # can create bucket
+
+
+
+    # can upload file to bucket
+
+
+
+    # can get sizes files in a bucket
+
+
+
+    # can copy a file from one bucket to another
+
+
+
+    # can copy all files larger than a given size in MB 
+    # from one bucket to another
+
+
